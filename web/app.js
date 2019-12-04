9ca40957fa72c9d3b71cd41e26cdade47ebee83c
Will Abrams <wabrams@users.noreply.github.com>
2019-12-04 10:54:03 -0700

    Fixed Upload for localhost
    //

--- web/app.js
+++ web/app.js
@@ -1,74 +1,10 @@
 var http = require("http");
 var fs = require('fs');
+var formidable = require('formidable');
 var port = 3000;
 var serverUrl = "127.0.0.1";
 var counter = 0;
-const {parse} = require('querystring');
 const request = require('request');
-
-function collectRequestData(request, callback)
-{
-  console.log("getting data");
-  const FORM_URLENCODED = 'application/x-www-form-urlencoded';
-  if(request.headers['content-type'] === FORM_URLENCODED)
-  {
-    let body = '';
-    request.on('data', chunk => {
-      body += chunk.toString();
-    });
-    request.on('end', () => {
-      callback(parse(body));
-    });
-  }
-  else
-  {
-    callback(null);
-  }
-}
-
-var server = http.createServer(function(req, res)
-{
-  counter++;
-  console.log("Request: " + req.url + " (" + counter + ")");
-
-  if(req.method === 'POST')
-  {
-      collectRequestData(req, result => {
-        console.log(result);
-        res.end(`Parsed data belonging to ${result.fname}`);
-    });
-  }
-  if(req.url == "/index.html")
-  {
-    fs.readFile("index.html", function(err, text)
-    {
-      res.setHeader("Content-Type", "text/html");
-      res.end(text);
-    });
-  }
-  else if(req.url.startsWith("/upload.html")) //this is also viable
-  {
-    fs.readFile("upload.html", function(err, text)
-    {
-      res.setHeader("Content-Type", "text/html");
-      res.end(text);
-    });
-  }
-  else if(req.url == "/download.html")
-  {
-    fs.readFile("download.html", function(err, text)
-    {
-      res.setHeader("Content-Type", "text/html");
-      res.end(text);
-    });
-  }
-  else
-  {
-    //TODO: err page goes here
-    res.setHeader("Content-Type", "text/html");
-    // res.end("<p>Hello World. Request counter: " + counter + ".</p>");
-  }
-});
 
 var mysql = require("mysql");
 var con = mysql.createConnection({
@@ -78,7 +14,93 @@ var con = mysql.createConnection({
 });
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected to DB!");
+  console.log("connected to MySQL");
+  con.query("USE filebox;", function (err, result) {
+    if (err) throw err;
+    console.log("connected to filebox DB");
+  });
+});
+
+var server = http.createServer(function(req, res)
+{
+  counter++;
+  console.log("Request: " + req.url + " (" + counter + ")");
+
+  if (req.url == '/upload/upf')
+  {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files)
+    {
+      var oldpath = files.filetoupload.path;
+      var newpath = "../fb/public/" + files.filetoupload.name;
+      // console.log(oldpath);
+      // console.log(newpath);
+      fs.rename(oldpath, newpath, function (err)
+      {
+        if (err) throw err;
+
+        con.query("INSERT INTO files (name) VALUES ("+'"'+files.filetoupload.name+'"'+");", function (err, result)
+        {
+           if (err) throw err;
+           console.log("added "+files.filetoupload.name+" to DB");
+         });
+
+         fs.readFile("upload.html", function(err, text)
+         {
+           res.setHeader("Content-Type", "text/html");
+           res.end(text);
+         });
+      });
+    });
+  }
+  else if(req.url == "/home" || req.url == "")
+  {
+    fs.readFile("index.html", function(err, text)
+    {
+      res.setHeader("Content-Type", "text/html");
+      res.end(text);
+    });
+  }
+  else if(req.url.startsWith("/upload")) //this is also viable
+  {
+    fs.readFile("upload.html", function(err, text)
+    {
+      res.setHeader("Content-Type", "text/html");
+      res.end(text);
+    });
+  }
+  else if(req.url == "/download")
+  {
+    fs.readFile("download.html", function(err, text)
+    {
+      res.setHeader("Content-Type", "text/html");
+      res.end(text);
+    });
+  }
+  else if(req.url == "/nav.html")
+  {
+    fs.readFile("nav.html", function(err, text)
+    {
+      res.setHeader("Content-Type", "text/html");
+      res.end(text);
+    });
+  }
+  else if(req.url == "/account.html")
+  {
+    fs.readFile("account.html", function(err, text)
+    {
+      res.setHeader("Content-Type", "text/html");
+      res.end(text);
+    });
+  }
+  else  //default case
+  {
+    fs.readFile("error.html", function(err, text)
+    {
+      res.setHeader("Content-Type", "text/html");
+      res.end(text);
+    });
+  }
 });
 
 console.log("Starting web server at " + serverUrl + ":" + port);
